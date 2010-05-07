b1989b9737cbb3b343669fe938214103159c0ea7
aj7 <ackbar@ackadij.com>
2010-05-07 13:21:12 +0100

    updated taxonconcept view

--- app/models/taxon_concept.rb
+++ app/models/taxon_concept.rb
@@ -5,7 +5,7 @@ class TaxonConcept < ActiveRecord::Base
   ### Plugins/Gems declarations
   ###
   acts_as_taggable
-  acts_as_taggable_on :properties, :status_types, :taxon_ranks  #the  tags and tag lists
+  acts_as_taggable_on :properties, :status_types , :taxon_ranks  #the  tags and tag lists
   acts_as_audited
   acts_as_nested_set
 
@@ -16,4 +16,9 @@ class TaxonConcept < ActiveRecord::Base
   #A TaxonConcept has got one TaxonName associated to it ; but one TaxonName can belongs_to_many TaxonConcepts
   belongs_to :taxon_name
 
+  ###
+  ### Validations
+  ###
+
+  validates_presence_of :taxon_name , :has_taxon_status
 end
